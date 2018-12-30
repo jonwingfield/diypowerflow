f7a905134aa52d631b9dbd1a108c6c3fb0af9fe6
jonwingfield <wingfield.jon@gmail.com>
2018-12-30 12:57:26 -0500

    JSHINT: Semicolons are ok ;)

--- project.js
+++ project.js
@@ -42,7 +42,7 @@ const darsksky_weight = 0.4;
 // Influx kWh queries
 const timezone = "Europe/Warsaw";
 const house_kwh_query = "SELECT sum(kwh) as kwh FROM energy WHERE (device = 'house' OR device= 'house2') AND time > now() - 1d GROUP BY time(1d) fill(0) TZ('"+timezone+"')";
-const grid_kwh_query = "SELECT sum(kwh) as kwh FROM energy WHERE (device = 'sdm120') AND time > now() - 1d GROUP BY time(1d) fill(0) TZ('"+timezone+"')"
+const grid_kwh_query = "SELECT sum(kwh) as kwh FROM energy WHERE (device = 'sdm120') AND time > now() - 1d GROUP BY time(1d) fill(0) TZ('"+timezone+"')";
 const solar_kwh_query = "select sum(wh)/1000 as kwh from (select watt/60 as wh from(SELECT MEAN(charging_power) as watt FROM pcm_query_general_status WHERE time > now() - 1d GROUP BY time(60s) fill(null) TZ('"+timezone+"') )) group by time(1d) fill(0) TZ('"+timezone+"')";
 const powerwall_soc_query = "SELECT last(ShuntSOC) FROM generic";
 
@@ -58,24 +58,24 @@ const solar_watt_topic = "solar/watt";
 //**************************************************
 //**************************************************
 
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 const diy_sun = require('./diy_sun');
 
-const mqtt = require('mqtt')
+const mqtt = require('mqtt');
 const mqtt_client = mqtt.connect('mqtt://'+mqtt_host);
 
-const Influx = require('influx')
+const Influx = require('influx');
 const influx = new Influx.InfluxDB({
   host: influx_host,
   database: 'powerwall'
-})
+});
 
 const influx_batrium = new Influx.InfluxDB({
   host: influx_host,
   database: 'batrium'
-})
+});
 
 
 var moment = require('moment');
@@ -156,7 +156,7 @@ mqtt_client.on('connect', () => {
   mqtt_client.subscribe(grid_watt_topic);
   mqtt_client.subscribe(powerwall_watt_topic);
   mqtt_client.subscribe(solar_watt_topic);
-})
+});
 
 mqtt_client.on("close",function(error){
   console.log("mqtt can't connect"+error);
@@ -164,30 +164,30 @@ mqtt_client.on("close",function(error){
   io.emit('grid', { message: 0 });
   io.emit('powerwall', { message: 0 });
   io.emit('solar', { message: 0 });
-})
+});
 
 mqtt_client.on('message', (topic, message) => {
   if(topic === house_watt_topic) {
-    house = parseInt(message.toString())
+    house = parseInt(message.toString());
     io.emit('house', { message: house+house2 });
   } else
     if(topic === powerwall_watt_topic) {
-      powerwall = parseInt(message.toString())
+      powerwall = parseInt(message.toString());
       io.emit('powerwall', { message: powerwall });
     } else
     if(topic === grid_watt_topic) {
-      grid = parseInt(message.toString())
+      grid = parseInt(message.toString());
       io.emit('grid', { message: grid });
     } else
     if(topic === house2_watt_topic) {
-      house2 = parseInt(message.toString())
+      house2 = parseInt(message.toString());
       io.emit('house', { message: house+house2 });
     } else
     if(topic === solar_watt_topic) {
-      solar = parseInt(message.toString())
+      solar = parseInt(message.toString());
       io.emit('solar', { message: solar });
     }
-})
+});
 
 app.get('/energy', function (req, res) {
   influx.query(house_kwh_query).then( house => {
@@ -203,7 +203,7 @@ app.get('/energy', function (req, res) {
       });
     });
   });
-})
+});
 
 app.get('/soc', function (req, res) {
   influx_batrium.query(powerwall_soc_query).then( soc => {
@@ -213,10 +213,10 @@ app.get('/soc', function (req, res) {
       ]
     );
   });
-})
+});
 
 
-app.use(express.static('static'))
+app.use(express.static('static'));
 
-server.listen(3333, () => console.log('DIY powerflow listening on port 3333!'))
+server.listen(3333, () => console.log('DIY powerflow listening on port 3333!'));
 
